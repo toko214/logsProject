3cf02e3c6b81dab490ed0df100c6d08caf624b33
toko214 <tomerpe1@gmail.com>
2018-06-15 16:47:11 +0300

    update2

--- start.py
+++ start.py
@@ -6,108 +6,101 @@ import os
 import chrome_ui
 
 
-# class ChromeCookiesApp(QtGui.QMainWindow, chrome_ui.cookies_ui):
-#     def __init__(self, parent=None, items=None, index=None):
-#         super(ChromeCookiesApp, self).__init__(parent)
-#         self.setupUi(self)
-#         self.index = index
-#         self.actionExit.triggered.connect(self.close)
-#         for i in items:
-#             for k in items[i]:
-#                 j = QtGui.QTreeWidgetItem(self.treeWidget, [i, k['name'], k['value'], k['expire'], k['time_created']])
-#                 self.treeWidget.addTopLevelItem(j)
-#
-#     def closeEvent(self, event):
-#         self.index.setDisabled(False)
-#         self.close()
-#
-#
-# class ChromeHistoryApp(QtGui.QMainWindow, chrome_ui.history_ui):
-#     def __init__(self, parent=None, items=None, index=None):
-#         super(ChromeHistoryApp, self).__init__(parent)
-#         self.setupUi(self)
-#         self.index = index
-#         for i in items:
-#             j = QtGui.QTreeWidgetItem(self.treeWidget, [items[i]['url'],str(items[i]['visit_time']),str(items[i]['visit_duration']),str(len(items[i]['visit_time']))])
-#             self.treeWidget.addTopLevelItem(j)
-#
-#     def closeEvent(self, event):
-#         self.index.setDisabled(False)
-#         self.close()
-#
-#
-# class ChromePassApp(QtGui.QMainWindow, chrome_ui.pass_Ui):
-#     def __init__(self, parent=None, items=None, index=None):
-#         super(ChromePassApp, self).__init__(parent)
-#         self.setupUi(self)
-#         self.index = index
-#         for i in items:
-#             j = QtGui.QTreeWidgetItem(self.treeWidget, [i[0], i[1], i[2]])
-#             self.treeWidget.addTopLevelItem(j)
-#
-#     def closeEvent(self, event):
-#         self.index.setDisabled(False)
-#         self.close()
-
-
-class GeneralApp(QtGui.QMainWindow, chrome_ui.general_ui):
+class GeneralSubApp(QtGui.QMainWindow, chrome_ui.general_ui):
     def __init__(self, parent=None, items=None, index=None, tree_items=None, name=""):
-        super(GeneralApp, self).__init__(parent)
+        super(GeneralSubApp, self).__init__(parent)
         self.setupUi(self, tree_items)
         self.index = index
+        self.setWindowTitle(name[0] + " " + name[1])
         self.actionExit.triggered.connect(self.close)
-        if name == "pass":
+        if name[1] == "Passwords":
             for i in items:
                 j = QtGui.QTreeWidgetItem(self.treeWidget, [i[0], i[1], i[2]])
                 self.treeWidget.addTopLevelItem(j)
-        if name == "history":
+        if name[1] == "History":
             for i in items:
                 j = QtGui.QTreeWidgetItem(self.treeWidget, [items[i]['url'],str(items[i]['visit_time']),str(items[i]['visit_duration']),str(len(items[i]['visit_time']))])
                 self.treeWidget.addTopLevelItem(j)
-        if name == "cookie":
+        if name[1] == "Cookies":
             for i in items:
                     for k in items[i]:
                         j = QtGui.QTreeWidgetItem(self.treeWidget, [i, k['name'], k['value'], k['expire'], k['time_created']])
                         self.treeWidget.addTopLevelItem(j)
 
     def closeEvent(self, event):
+        print "ASd"
         self.index.setDisabled(False)
-        self.close()
 
-class ChromeApp(QtGui.QMainWindow, chrome_ui.Ui_MainWindow):
-    def __init__(self,parent=None, index=None):
-        super(ChromeApp, self).__init__(parent)
-        self.setupUi(self)
+
+class GeneralApp(QtGui.QMainWindow, chrome_ui.general_apps_ui):
+    def __init__(self,parent=None, index=None, tree_items=None, name=None):
+        super(GeneralApp, self).__init__(parent)
+        self.setupUi(self, tree_items, name)
         self.treeWidget.itemDoubleClicked.connect(self.tree_handle)
         self.index = index
-        self.statusbar.showMessage("Chrome Apps | 4")
         self.actionExit.triggered.connect(self.close)
 
     def tree_handle(self, index):
         if not index.isDisabled():
-            if index.text(0) == "Chrome Passwords":
-                index.setDisabled(True)
-                passwords = MainEngine.ce.ChromeEngine().get_chrome_saved_password()
-                tree_items = [[0, "Website"], [1, "Username"], [2, "Password"]]
-                form = GeneralApp(self, items=passwords,index=index, tree_items=tree_items, name="pass")
-                form.show()
-            if index.text(0) == "Chrome History":
-                index.setDisabled(True)
-                history = MainEngine.ce.ChromeEngine().get_chrome_history()
-                tree_items = [[0, "Url"], [1, "Visit Time"], [2, "Visit Duration"], [3, "Times Visited"]]
-                form = GeneralApp(self, items=history,index=index, tree_items= tree_items, name="history")
-                form.show()
-            if index.text(0) == "Chrome Cookies":
-                index.setDisabled(True)
-                cookies = MainEngine.ce.ChromeEngine().get_cookies()
-                tree_items = [[0, "Url"], [1, "Name"], [2, "Value"], [3, "Expire"], [4, "Time Created"]]
-                form = GeneralApp(self, items=cookies, index=index, tree_items=tree_items, name="cookie")
-                form.show()
-
+            name = index.text(0).split(" ")
+            if name[0] == "Chrome":
+                if name[1] == "Passwords":
+                    index.setDisabled(True)
+                    passwords = MainEngine.ce.ChromeEngine().get_chrome_saved_password()
+                    tree_items = [[0, "Website"], [1, "Username"], [2, "Password"]]
+                    form = GeneralSubApp(self, items=passwords,index=index, tree_items=tree_items, name=name)
+                    form.show()
+                if name[1] == "History":
+                    index.setDisabled(True)
+                    history = MainEngine.ce.ChromeEngine().get_chrome_history()
+                    tree_items = [[0, "Url"], [1, "Visit Time"], [2, "Visit Duration"], [3, "Times Visited"]]
+                    form = GeneralSubApp(self, items=history,index=index, tree_items= tree_items, name=name)
+                    form.show()
+                if name[1] == "Cookies":
+                    index.setDisabled(True)
+                    cookies = MainEngine.ce.ChromeEngine().get_cookies()
+                    tree_items = [[0, "Url"], [1, "Name"], [2, "Value"], [3, "Expire"], [4, "Time Created"]]
+                    form = GeneralSubApp(self, items=cookies, index=index, tree_items=tree_items, name=name)
+                    form.show()
 
     def closeEvent(self, event):
+        print "asd"
         self.index.setDisabled(False)
-        self.close()
+
+
+# class ChromeApp(QtGui.QMainWindow, chrome_ui.Ui_MainWindow):
+#     def __init__(self,parent=None, index=None, tree_items=None, name=None):
+#         super(ChromeApp, self).__init__(parent)
+#         self.setupUi(self)
+#         self.treeWidget.itemDoubleClicked.connect(self.tree_handle)
+#         self.index = index
+#         self.actionExit.triggered.connect(self.close)
+#
+#     def tree_handle(self, index):
+#         if not index.isDisabled():
+#             if index.text(0) == "Chrome Passwords":
+#                 index.setDisabled(True)
+#                 passwords = MainEngine.ce.ChromeEngine().get_chrome_saved_password()
+#                 tree_items = [[0, "Website"], [1, "Username"], [2, "Password"]]
+#                 form = GeneralSubApp(self, items=passwords,index=index, tree_items=tree_items, name="pass")
+#                 form.show()
+#             if index.text(0) == "Chrome History":
+#                 index.setDisabled(True)
+#                 history = MainEngine.ce.ChromeEngine().get_chrome_history()
+#                 tree_items = [[0, "Url"], [1, "Visit Time"], [2, "Visit Duration"], [3, "Times Visited"]]
+#                 form = GeneralSubApp(self, items=history,index=index, tree_items= tree_items, name="history")
+#                 form.show()
+#             if index.text(0) == "Chrome Cookies":
+#                 index.setDisabled(True)
+#                 cookies = MainEngine.ce.ChromeEngine().get_cookies()
+#                 tree_items = [[0, "Url"], [1, "Name"], [2, "Value"], [3, "Expire"], [4, "Time Created"]]
+#                 form = GeneralSubApp(self, items=cookies, index=index, tree_items=tree_items, name="cookie")
+#                 form.show()
+#
+#
+#     def closeEvent(self, event):
+#         self.index.setDisabled(False)
+#         self.close()
 
 
 class MainApp(QtGui.QMainWindow, design.Ui_MainWindow):
@@ -125,7 +118,8 @@ class MainApp(QtGui.QMainWindow, design.Ui_MainWindow):
         if not index.isDisabled():
             if index.text(0) == "Chrome":
                 index.setDisabled(True)
-                form = ChromeApp(self, index)
+                tree_items = [[0, "Password"], [1, "Cache"], [2, "Bookmarks"], [3, "History"], [4, "Cookies"]]
+                form = GeneralApp(self, index=index, tree_items=tree_items, name=index.text(0))
                 form.show()
 
     def browse_file(self):

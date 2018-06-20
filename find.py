9788e0c0dc1ed38d226c09f02c8945de767f1b3c
toko214 <tomerpe1@gmail.com>
2018-06-20 15:41:08 +0300

    updat3

---
+++ find.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'find.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(367, 123)
+        MainWindow.setMinimumSize(QtCore.QSize(367, 123))
+        MainWindow.setMaximumSize(QtCore.QSize(367, 123))
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(5, 15, 71, 16))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label.setFont(font)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(70, 15, 201, 20))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(280, 15, 75, 23))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.checkBox = QtGui.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(5, 50, 111, 17))
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.checkBox_2 = QtGui.QCheckBox(self.centralwidget)
+        self.checkBox_2.setGeometry(QtCore.QRect(5, 80, 131, 17))
+        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
+        self.groupBox = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(150, 50, 121, 41))
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.radioButton = QtGui.QRadioButton(self.groupBox)
+        self.radioButton.setGeometry(QtCore.QRect(10, 20, 82, 17))
+        self.radioButton.setObjectName(_fromUtf8("radioButton"))
+        self.radioButton_2 = QtGui.QRadioButton(self.groupBox)
+        self.radioButton_2.setGeometry(QtCore.QRect(60, 20, 82, 17))
+        self.radioButton_2.setObjectName(_fromUtf8("radioButton_2"))
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(280, 50, 75, 23))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setSizeGripEnabled(False)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Find", None))
+        self.label.setText(_translate("MainWindow", "Find What:", None))
+        self.pushButton.setText(_translate("MainWindow", "Find Next", None))
+        self.checkBox.setText(_translate("MainWindow", "Match whole word", None))
+        self.checkBox_2.setText(_translate("MainWindow", "Match part of the word", None))
+        self.groupBox.setTitle(_translate("MainWindow", "Direction", None))
+        self.radioButton.setText(_translate("MainWindow", "Up", None))
+        self.radioButton_2.setText(_translate("MainWindow", "Down", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Cancle", None))
+
--- start.py
+++ start.py
@@ -4,32 +4,121 @@ import design
 import MainEngine
 import os
 import chrome_ui
+import threading
+import xlwt
+import find
+
+
+class FindApp(QtGui.QMainWindow, find.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(FindApp, self).__init__(parent)
+        self.setupUi(self)
+        self.parent_window = parent
+        self.start_index = 0
+        self.pushButton.clicked.connect(self.find_in_tree)
+        self.pushButton_2.clicked.connect(self.close)
+        self.items = []
+        self.current_key = ""
+        self.no_items_found = False
+
+    def find_in_tree(self):
+        key = self.lineEdit.text()
+        if key != self.current_key:
+            self.no_items_found = False
+            self.current_key = key
+            self.items = []
+            for i in range(self.parent_window.header_count):
+                items = self.parent_window.treeWidget.findItems(key, QtCore.Qt.MatchContains, i)
+                if items:
+                    self.items.append([i, items])
+            if not self.items:
+                message_box(self=self, title="No Items Found", txt="No Items Found With this keyword")
+                self.no_items_found = True
+            else:
+                self.progress()
+        else:
+            if not self.no_items_found:
+                if self.items:
+                    self.progress()
+                else:
+                    message_box(self=self, title="No More Items", txt="No More Items Found")
+            else:
+                message_box(self=self, title="No Items Found", txt="No Items Found With this keyword")
+
+    def progress(self):
+        for j in self.items:
+            if j[1]:
+                self.parent_window.treeWidget.setCurrentItem(j[1][0], j[0])
+                self.items[self.items.index(j)][1].remove(j[1][0])
+                print self.items
+                if not j[1]:
+                    self.items.remove(j)
+                break
+            else:
+                self.items.remove(j)
+                print self.items
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Return:
+            self.pushButton.click()
 
 
 class GeneralSubApp(QtGui.QMainWindow, chrome_ui.general_ui):
     def __init__(self, parent=None, items=None, index=None, tree_items=None, name=""):
         super(GeneralSubApp, self).__init__(parent)
-        self.setupUi(self, tree_items)
+        self.setupUi(self, items, name)
+        self.header_count = len(items)
+        self.items_count = len(tree_items)
         self.index = index
-        self.setWindowTitle(name[0] + " " + name[1])
         self.actionExit.triggered.connect(self.close)
-        if name[1] == "Passwords":
-            for i in items:
-                j = QtGui.QTreeWidgetItem(self.treeWidget, [i[0], i[1], i[2]])
-                self.treeWidget.addTopLevelItem(j)
-        if name[1] == "History":
-            for i in items:
-                j = QtGui.QTreeWidgetItem(self.treeWidget, [items[i]['url'],str(items[i]['visit_time']),str(items[i]['visit_duration']),str(len(items[i]['visit_time']))])
-                self.treeWidget.addTopLevelItem(j)
-        if name[1] == "Cookies":
-            for i in items:
-                    for k in items[i]:
-                        j = QtGui.QTreeWidgetItem(self.treeWidget, [i, k['name'], k['value'], k['expire'], k['time_created']])
-                        self.treeWidget.addTopLevelItem(j)
+        self.actionSave_Selected_Items.triggered.connect(self.tree_to_xl)
+        self.actionSave_Selected_Items.setShortcut(QtGui.QKeySequence('Ctrl+S'))
+        self.actionFind.triggered.connect(self.open_find_ui)
+        self.actionFind.setShortcut(QtGui.QKeySequence('Ctrl+F'))
+        self.actionSelect_All.triggered.connect(self.treeWidget.selectAll)
+        self.actionSelect_All.setShortcut(QtGui.QKeySequence('Ctrl+A'))
+        self.actionDeselect_All.triggered.connect(self.treeWidget.clearSelection)
+        self.actionDeselect_All.setShortcut(QtGui.QKeySequence('Ctrl+D'))
+        self.children_windows = []
+        for i in tree_items:
+            j = QtGui.QTreeWidgetItem(self.treeWidget, i)
+            self.treeWidget.addTopLevelItem(j)
 
     def closeEvent(self, event):
-        print "ASd"
         self.index.setDisabled(False)
+        for i in self.children_windows:
+            i.close()
+
+    def open_find_ui(self):
+        form = FindApp(self)
+        self.children_windows.append(form)
+        form.show()
+
+    def tree_to_xl(self):
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet('Users')
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True
+        columns = []
+        for i in range(self.header_count):
+            columns.append(str(self.treeWidget.headerItem().text(i)))
+        for col_num in range(self.header_count):
+            ws.write(0, col_num, columns[col_num], font_style)
+            ws.col(col_num).width = 256 * 40
+        font_style = xlwt.XFStyle()
+        if len(self.treeWidget.selectedItems()) == self.items_count:
+            for row_num in range(self.treeWidget.topLevelItemCount()):
+                for col_num in range(self.header_count):
+                    ws.write(row_num + 1, col_num, str(self.treeWidget.topLevelItem(row_num).text(col_num)), font_style)
+        else:
+            items = self.treeWidget.selectedItems()
+            x = 0
+            for item in items:
+                for col_num in range(self.header_count):
+                    ws.write(x + 1, col_num, str(item.text(col_num)),
+                             font_style)
+                x += 1
+        wb.save("asd.xls")
 
 
 class GeneralApp(QtGui.QMainWindow, chrome_ui.general_apps_ui):
@@ -40,67 +129,46 @@ class GeneralApp(QtGui.QMainWindow, chrome_ui.general_apps_ui):
         self.index = index
         self.actionExit.triggered.connect(self.close)
 
+    def start_tree_thread(self, index):
+        t = threading.Thread(target=self.tree_handle, args=(index,))
+        t.start()
+
     def tree_handle(self, index):
         if not index.isDisabled():
             name = index.text(0).split(" ")
             if name[0] == "Chrome":
-                if name[1] == "Passwords":
+                if name[1] == "Password":
                     index.setDisabled(True)
                     passwords = MainEngine.ce.ChromeEngine().get_chrome_saved_password()
-                    tree_items = [[0, "Website"], [1, "Username"], [2, "Password"]]
-                    form = GeneralSubApp(self, items=passwords,index=index, tree_items=tree_items, name=name)
+                    tree_items = []
+                    for i in passwords:
+                        tree_items.append([i[0], i[1], i[2]])
+                    items = [[0, "Website"], [1, "Username"], [2, "Password"]]
+                    form = GeneralSubApp(self, items=items,index=index, tree_items=tree_items, name=name)
                     form.show()
                 if name[1] == "History":
                     index.setDisabled(True)
                     history = MainEngine.ce.ChromeEngine().get_chrome_history()
-                    tree_items = [[0, "Url"], [1, "Visit Time"], [2, "Visit Duration"], [3, "Times Visited"]]
-                    form = GeneralSubApp(self, items=history,index=index, tree_items= tree_items, name=name)
+                    tree_items = []
+                    for i in history:
+                        tree_items.append([history[i]['url'], str(history[i]['visit_time']),str(history[i]['visit_duration']), str(len(history[i]['visit_time']))])
+                    items = [[0, "Url"], [1, "Visit Time"], [2, "Visit Duration"], [3, "Times Visited"]]
+                    form = GeneralSubApp(self, items=items,index=index, tree_items= tree_items, name=name)
                     form.show()
                 if name[1] == "Cookies":
                     index.setDisabled(True)
                     cookies = MainEngine.ce.ChromeEngine().get_cookies()
-                    tree_items = [[0, "Url"], [1, "Name"], [2, "Value"], [3, "Expire"], [4, "Time Created"]]
-                    form = GeneralSubApp(self, items=cookies, index=index, tree_items=tree_items, name=name)
+                    tree_items = []
+                    for i in cookies:
+                        for k in cookies[i]:
+                            tree_items.append([i, k['name'], k['value'], k['expire'], k['time_created']])
+                    items = [[0, "Url"], [1, "Name"], [2, "Value"], [3, "Expire"], [4, "Time Created"]]
+                    form = GeneralSubApp(self, items=items, index=index, tree_items=tree_items, name=name)
                     form.show()
+            #if name[0] == "Firefox":
 
     def closeEvent(self, event):
-        print "asd"
         self.index.setDisabled(False)
-
-
-# class ChromeApp(QtGui.QMainWindow, chrome_ui.Ui_MainWindow):
-#     def __init__(self,parent=None, index=None, tree_items=None, name=None):
-#         super(ChromeApp, self).__init__(parent)
-#         self.setupUi(self)
-#         self.treeWidget.itemDoubleClicked.connect(self.tree_handle)
-#         self.index = index
-#         self.actionExit.triggered.connect(self.close)
-#
-#     def tree_handle(self, index):
-#         if not index.isDisabled():
-#             if index.text(0) == "Chrome Passwords":
-#                 index.setDisabled(True)
-#                 passwords = MainEngine.ce.ChromeEngine().get_chrome_saved_password()
-#                 tree_items = [[0, "Website"], [1, "Username"], [2, "Password"]]
-#                 form = GeneralSubApp(self, items=passwords,index=index, tree_items=tree_items, name="pass")
-#                 form.show()
-#             if index.text(0) == "Chrome History":
-#                 index.setDisabled(True)
-#                 history = MainEngine.ce.ChromeEngine().get_chrome_history()
-#                 tree_items = [[0, "Url"], [1, "Visit Time"], [2, "Visit Duration"], [3, "Times Visited"]]
-#                 form = GeneralSubApp(self, items=history,index=index, tree_items= tree_items, name="history")
-#                 form.show()
-#             if index.text(0) == "Chrome Cookies":
-#                 index.setDisabled(True)
-#                 cookies = MainEngine.ce.ChromeEngine().get_cookies()
-#                 tree_items = [[0, "Url"], [1, "Name"], [2, "Value"], [3, "Expire"], [4, "Time Created"]]
-#                 form = GeneralSubApp(self, items=cookies, index=index, tree_items=tree_items, name="cookie")
-#                 form.show()
-#
-#
-#     def closeEvent(self, event):
-#         self.index.setDisabled(False)
-#         self.close()
 
 
 class MainApp(QtGui.QMainWindow, design.Ui_MainWindow):
@@ -110,16 +178,25 @@ class MainApp(QtGui.QMainWindow, design.Ui_MainWindow):
         # self.pushButton_2.clicked.connect(self.browse_file)
         # self.pushButton.clicked.connect(self.analyze)
         self.dir_path = os.getcwd() + "\logs\\"
+        self.actionExit.triggered.connect(self.close)
         self.treeWidget_2.itemDoubleClicked.connect(self.check_index)
         if not os.path.exists(os.path.dirname(self.dir_path)):
             os.makedirs(os.path.dirname(self.dir_path))
+        self.dialog = QtGui.QDialog(self)
+        self.dialog.setFocusPolicy(QtCore.Qt.StrongFocus)
 
     def check_index(self,index):
         if not index.isDisabled():
-            if index.text(0) == "Chrome":
+            name = index.text(0)
+            if name == "Chrome":
                 index.setDisabled(True)
                 tree_items = [[0, "Password"], [1, "Cache"], [2, "Bookmarks"], [3, "History"], [4, "Cookies"]]
-                form = GeneralApp(self, index=index, tree_items=tree_items, name=index.text(0))
+                form = GeneralApp(self, index=index, tree_items=tree_items, name=name)
+                form.show()
+            if name == "Firefox":
+                index.setDisabled(True)
+                tree_items = [[0, "Password"], [1, "Bookmarks"], [2, "History"], [3, "Cookies"]]
+                form = GeneralApp(self, index=index, tree_items=tree_items, name=name)
                 form.show()
 
     def browse_file(self):
@@ -127,9 +204,44 @@ class MainApp(QtGui.QMainWindow, design.Ui_MainWindow):
         self.lineEdit.setText(file)
         QtGui.QTreeWidgetItem.checkState()
 
+    def focusOutEvent(self, event):
+        print "Asd"
+        self.setFocus(True)
+        # self.activateWindow()
+        # self.raise_()
+        # self.show()
+
+
+# def changedFocusSlot(old, new):
+#     print "old   " + str(old)
+#     print "new   " + str(new)
+#     window = QtGui.QApplication.activeWindow()
+#     if window != None and old != None and new != None:
+#         print window.windowTitle()
+
+
+    #     window.setWindowState(window.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
+    # # if (now!=None):
+    # #     # QtGui.QApplication.activeWindow().raise_()
+    # #     # QtGui.QApplication.activeWindow().show()
+    # #     now.setWindowState(now.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
+    # #     now.activateWindow()
+    # #     # now.raise_()
+    # #     # now.show()
+    # #     # now.activateWindow()
+
+def message_box(self=None, title="", txt=""):
+    msg = QtGui.QMessageBox(self)
+    msg.setIcon(QtGui.QMessageBox.Information)
+    msg.setText(txt)
+    # msg.setInformativeText("This is additional information")
+    msg.setWindowTitle(title)
+    # msg.setDetailedText("The details are as follows:")
+    msg.show()
 
 def main():
     app = QtGui.QApplication(sys.argv)
+    #QtCore.QObject.connect(app, QtCore.SIGNAL("focusChanged(QWidget *, QWidget *)"), changedFocusSlot)
     form = MainApp()
     form.show()
     app.exec_()
